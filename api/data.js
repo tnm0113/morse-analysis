dfffd783445b16f44fe3e88a170fabadfc0c203d
tnm0113 <tnm0113@gmail.com>
2025-05-07 21:59:39 +0700

    fix query

--- api/data.js
+++ api/data.js
@@ -7,8 +7,8 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
     const { data, error } = await supabase
-        .from("transfers")
-        .select("holder_address, nft_id, days_since, rarity");
+        .from("holders")
+        .select("address, nft_id, days_since, rarity");
     if (error) return res.status(500).json({ error: error.message });
     res.status(200).json(data);
 }
